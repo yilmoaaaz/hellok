27d26404ab0b007859c4274920772e250bfad3e6
yilmoaaaz <yilmoaaz@gmail.com>
2025-02-27 22:46:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
